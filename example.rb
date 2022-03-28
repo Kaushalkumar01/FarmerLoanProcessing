dc587e906b4a8ea35d1c1831175da6d58dfca96d
Kaushalkumar01 <88927589+Kaushalkumar01@users.noreply.github.com>
2022-03-28 10:52:10 +0530

    Create example.rb

---
+++ example.rb
@@ -0,0 +1,35 @@
+class Developer
+
+
+
+class << self
+
+def backend
+
+"Backend Developer"
+
+end
+
+end
+
+end
+
+
+
+-----
+
+
+
+class Object
+
+def metaclass_example
+
+class << self
+
+self
+
+end
+
+end
+
+end
